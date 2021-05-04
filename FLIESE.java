e8423b9c0554b9afe08c47b3afcfdbdfcb377124
emweisenburger <emma@eecrw.net>
2021-05-04 09:41:19 +0200

    Erste Programmierungen in OBERFLÄCHE und FLIESE

--- FLIESE.java
+++ FLIESE.java
@@ -5,11 +5,100 @@
  * @emweisenburger / @kathischmid
  * @version1_04.05.2021
  */
-public class FLIESE
+
+
+import java.awt.*;
+import javax. swing. *;
+
+class FLIESE
 {
+    /** Das Anzeigefenster. */
+    private JFrame fenster;
+    
+    /** Anzeigegröße für das Rumpfelement */
+    private static final int groesse = OBERFLAECHE. RasterGroesseGeben ();
+
+    /** Interna */
+    private int x;
+    private int y;
+    private JComponent anzeige;
+
     /**
-     * Constructor for objects of class FLIESE
+     * Standardkonstruktor für Objekte der Klasse KASSENSYMBOL.
+     * Er erzeugt ein scharzes Rechteck in der linken oberen Ecke des Fensters.
+     * Das Fenster wird bei Bedarf angelegt.
      */
+    FLIESE()
+    {
+        fenster = OBERFLAECHE. FensterGeben ();
+        anzeige = new JComponent () 
+        {
+            // Stellt das Rumpfelement auf dem Fenster dar.
+            public void paintComponent (Graphics g)
+            {
+                g. clearRect (0, 0, groesse, groesse);
+                g. setColor (Color. yellow);
+                g. fillRect (0, 0, groesse - 1, groesse - 1);
+                g. setColor (new Color (50, 200, 0));
+                g. drawRect (0, 0, groesse - 1, groesse - 1);
+                g. drawRect (1, 1, groesse - 3, groesse - 3);
+                
+            }
+
+        };
+        anzeige. setVisible (true);
+        anzeige. setSize (groesse, groesse);
+        fenster. add (anzeige, 0);
+        PositionSetzen (0, 0);
+    }
+
+    /**
+     * Setzt die Farbe
+     */
+    void FarbeSetzen ()
+    {
+        
+    }
     
     
+    /**
+     * Setzt die Position des Rumpfelements. Der Ursprung liegt in der Mitte des
+     * Fensters, die y-Achse zeigt nach unten. (x /y) bedeutet das
+     * K&auml;stchen rechts unterhalb der Gitterlinien.
+     * @param x x-Position
+     * @param y y-Position
+     */
+    public void PositionSetzen (int x, int y)
+    {
+        this. x = x;
+        this. y = y;
+        anzeige. setLocation (OBERFLAECHE. FensterBreiteGeben () / 2 + x * groesse, OBERFLAECHE. FensterHoeheGeben () / 2 + y * groesse);
+    }
+
+    /**
+     * Gibt den X-Wert der Position des Kopfelements.
+     * @return x-Position
+     */
+    int XPositionGeben ()
+    {
+        return x;
+    }
+
+    /**
+     * Gibt den Y-Wert der Position des Kopfelements.
+     * @return y-Position
+     */
+    int YPositionGeben ()
+    {
+        return y;
+    }
+
+    /**
+     * Entfernt die Figur aus der Anzeige
+     */
+    public void Entfernen ()
+    {
+        (OBERFLAECHE. FensterGeben ()). remove (anzeige);
+        (OBERFLAECHE. FensterGeben ()). repaint();
+    }
 }
---
+++ OBERFLAECHE.java
@@ -0,0 +1,103 @@
+
+
+import java. awt. *;
+import javax. swing. *;
+
+/**
+ * Verwaltet das Ausgabefenster.
+ * 
+ * @author Albert Wiedemann
+ * @version 1.0
+ */
+class OBERFLAECHE
+{
+    /** Anzeigegröße für eine Rasterkachel */
+    private static final int rasterGroesse = 40;
+    /** Höhe des Anzeigefensters */
+    private static final int hoeheFenster = rasterGroesse * 20;
+    /** Breite des Anzeigefensters */
+    private static final int breiteFenster = rasterGroesse * 26;
+    private static OBERFLAECHE o = null;
+    private JFrame fenster = null;
+
+    /**
+     * Baut die Bedienoberfläche auf
+     */
+    private OBERFLAECHE ()
+    {
+        fenster = new JFrame ("Zeichenfläche");
+        fenster. setResizable (false);
+        fenster. setVisible (true);
+        fenster. setLayout (null);
+        fenster. getContentPane (). setBackground (new Color (240, 240, 240));
+        Insets i = fenster.getInsets();
+        fenster. setSize (breiteFenster, hoeheFenster + i.top);         // Ausgleich für Fenstertitel
+        JComponent hintergrund = new JComponent ()
+        {
+             /**
+             * Zeichnet das Hintergrundmuster.
+             */
+            public void paintComponent (Graphics g)
+            {
+                g. setColor (Color. lightGray);
+                for (int i = 1; i <= hoeheFenster / (rasterGroesse * 2); i++)
+                {
+                    g. drawLine (0, hoeheFenster / 2 + i * rasterGroesse, breiteFenster - 1, hoeheFenster / 2 + i * rasterGroesse);
+                    g. drawLine (0, hoeheFenster / 2 - i * rasterGroesse, breiteFenster - 1, hoeheFenster / 2 - i * rasterGroesse);
+                }
+                for (int i = 1; i <= breiteFenster / (rasterGroesse * 2); i++)
+                {
+                    g. drawLine (breiteFenster / 2 + i * rasterGroesse, 0, breiteFenster / 2 + i * rasterGroesse, hoeheFenster - 1);
+                    g. drawLine (breiteFenster / 2 - i * rasterGroesse, 0, breiteFenster / 2 - i * rasterGroesse, hoeheFenster - 1);
+                }
+                g. setColor (Color. black);
+                g. drawLine (0, hoeheFenster / 2, breiteFenster - 1, hoeheFenster / 2);
+                g. drawLine (breiteFenster / 2, 0, breiteFenster / 2, hoeheFenster - 1);
+            }
+        };
+        hintergrund. setVisible (true);
+        hintergrund. setSize (breiteFenster, hoeheFenster);
+        hintergrund. setLocation (0, 0);
+        fenster. add (hintergrund);
+    }
+
+    /**
+     * Gibt das Ausgabefenster zurück und erzeugt es gegebenenfalls.
+     * @return Ausgabefenster
+     */
+    static JFrame FensterGeben ()
+    {
+        if (o == null)
+        {
+            o = new OBERFLAECHE ();
+        }
+        return o. fenster;
+    }
+
+    /**
+     * Gibt die Rastergröße zurück.
+     * @return Ausgabefenster
+     */
+    static int RasterGroesseGeben ()
+    {
+        return rasterGroesse;
+    }
+
+    /**
+     * Gibt die Breite des Fensters zurück.
+     * @return Fensterbreite
+     */
+    static int FensterBreiteGeben ()
+    {
+        return breiteFenster;
+    }
+
+    /**
+     * Gibt die Höhe des Fensters zurück.
+     * @return Fensterhöhe
+     */
+    static int FensterHoeheGeben ()
+    {
+        return hoeheFenster;
+    }
+}
