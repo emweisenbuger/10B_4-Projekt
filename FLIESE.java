3950a09a0d1495dbd5b9898c6928840dbd65d5a6
emweisenburger <emma@eecrw.net>
2021-05-04 09:21:13 +0200

    Klasse Fliese eingefügt

---
+++ FLIESE.java
@@ -0,0 +1,15 @@
+
+/**
+ * Das Symbol der Fliesen.
+ *
+ * @emweisenburger / @kathischmid
+ * @version1_04.05.2021
+ */
+public class FLIESE
+{
+    /**
+     * Constructor for objects of class FLIESE
+     */
+    
+    
+}
