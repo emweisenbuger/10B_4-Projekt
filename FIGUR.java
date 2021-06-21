5d4a20777b45e7fe4418e0c4f8118e5b4a0f36c2
EmilLenzer <emil.lenzer@gmx.de>
2021-06-21 21:31:48 +0200

    Spielfiguren werden angelegt und Spielzug wird programmiert.

--- FIGUR.java
+++ FIGUR.java
@@ -1,22 +1,34 @@
 
-
-import java.awt.Color;
-import java.awt.Graphics;
-class FIGUR
+public class FIGUR
 {
    public static int x;
    public static int y;
-   static Color farbe;
-   FIGUR(int x, int y)
+   String farbe;
+   KREIS fuellung;
+   KREIS rahmen;
+   public FIGUR(String farbe) 
    {
        this.x=x;
        this.y=y;
+       this.farbe=farbe;
+
+       rahmen = new KREIS();
+       rahmen.FarbeSetzen("schwarz");
+       fuellung = new KREIS();
+       fuellung.RadiusSetzen(15);
+       fuellung.FarbeSetzen(farbe);
+    }
+   public void PositionSetzen(int x, int y)
+   {
+       this.x=x;
+       this.y=y;
+       rahmen.MittelpunktSetzen(x,y);
        
-    }
-    
-    public static void draw (Graphics g)
-    {
-       g.setColor(Color.BLUE); 
-       g.fillOval(x, y, 35, 35);
-    }
+       fuellung.MittelpunktSetzen(x,y);
+   }
+   public void Highlight()
+   {
+       rahmen.FarbeSetzen("magenta");
+   }
+ 
 }
--- SPIEL.java
+++ SPIEL.java
@@ -1,14 +1,16 @@
- 
+import java.awt.*;
+import java.awt.event.*;
 /**
  * Zusammensetzung des Spielbretts
  * 
  * @version1_20.06.2021
  */
 
-public class SPIEL 
-
-{
-    int [][] spielfeld = {{ 71,72,0,0,1,2,3,0,0,41,42},
+public class SPIEL extends EREIGNISSE
+ 
+{ 
+    int augenzahl;
+    int [][] spielfeld = { { 71,72,0,0,1,2,3,0,0,41,42},
                            { 73,74,0,0,40,45,4,0,0,43,44},
                            {0,0,0,0,39,46,5,0,0,0,0},
                            {0,0,0,0,38,47,6,0,0,0,0},
@@ -22,9 +24,35 @@ public class SPIEL
                         };   
    KREIS [] kreis;
    
-    
+   // Spielfiguren:
+   FIGUR Blau1;
+   FIGUR Blau2;
+   FIGUR Blau3;
+   FIGUR Blau4;
+   FIGUR Rot1;
+   FIGUR Rot2;
+   FIGUR Rot3;
+   FIGUR Rot4;
+   FIGUR Gelb1;
+   FIGUR Gelb2;
+   FIGUR Gelb3;
+   FIGUR Gelb4;
+   FIGUR Gruen1;
+   FIGUR Gruen2;
+   FIGUR Gruen3;
+   FIGUR Gruen4;
+   
+   int SpielerAnDerReihe;
+   int SpielerInHausR;
+   int SpielerInHausG;
+   int SpielerInHausB;
+   int SpielerInHausY;
+   int selectedSpieler;
+ 
     public SPIEL()
     {
+        
+       
         kreis = new KREIS[80];
         for (int i = 0; i < kreis. length; i++)
         {
@@ -55,7 +83,7 @@ public class SPIEL
         {
             kreis[i].FarbeSetzen("rot");
         }
-        kreis[33].FarbeSetzen("orange");
+        kreis[33].FarbeSetzen("orange"); 
         
         for (int zeile = 0;zeile <= 10;zeile++)
         {
@@ -70,7 +98,99 @@ public class SPIEL
             }
         }
     }
-}        
+    
+    // Bereich für Instanzierung der Spielfiguren
+    
+    // BLAU
+    Blau1 = new FIGUR("blau");
+    Blau1.PositionSetzen(200+0*60,120+9*60);
+    Blau2 = new FIGUR("blau");
+    Blau2.PositionSetzen(200+1*60,120+9*60);
+    Blau3 = new FIGUR("blau");
+    Blau3.PositionSetzen(200+0*60,120+10*60);
+    Blau4 = new FIGUR("blau");
+    Blau4.PositionSetzen(200+1*60,120+10*60);
+    // GELB
+    Gelb1 = new FIGUR("gelb");
+    Gelb1.PositionSetzen(200+9*60,120+9*60);
+    Gelb2 = new FIGUR("gelb");
+    Gelb2.PositionSetzen(200+10*60,120+9*60);
+    Gelb3 = new FIGUR("gelb");
+    Gelb3.PositionSetzen(200+9*60,120+10*60);
+    Gelb4 = new FIGUR("gelb");
+    Gelb4.PositionSetzen(200+10*60,120+10*60);
+    // GRUEN
+    Gruen1 = new FIGUR("gruen");
+    Gruen1.PositionSetzen(200+9*60,120+0*60);
+    Gruen2 = new FIGUR("gruen");
+    Gruen2.PositionSetzen(200+10*60,120+0*60);
+    Gruen3 = new FIGUR("gruen");
+    Gruen3.PositionSetzen(200+9*60,120+1*60);
+    Gruen4 = new FIGUR("gruen");
+    Gruen4.PositionSetzen(200+10*60,120+1*60);
+    // ROT
+    Rot1 = new FIGUR("rot");
+    Rot1.PositionSetzen(200+0*60,120+0*60);
+    Rot2 = new FIGUR("rot");
+    Rot2.PositionSetzen(200+1*60,120+0*60);
+    Rot3 = new FIGUR("rot");
+    Rot3.PositionSetzen(200+0*60,120+1*60);
+    Rot4 = new FIGUR("rot");
+    Rot4.PositionSetzen(200+1*60,120+1*60);
+
+    // Spieler an der Reihe: Rot = 1; Grün = 2; Rot = 3; Gelb = 4;
+    SpielerAnDerReihe = 1;
+    SpielerInHausR = 4;
+    
+ }     
+ @Override
+    void TasteGedrueckt (char welche)
+ {
+        
+        if ((welche == 'W') || (welche == 'w'))
+        {
+            // System.out.println("Hier wird gewürfelt");
+         double augenzahlungerundet=Math.random()*6;
+        
+           int augenzahlfinal=(int)augenzahlungerundet+1;
+           System.out.println("gewürfelte augenzahl: "+ augenzahlfinal);
+           augenzahl = augenzahlfinal;
+           spielzug();
+        
+        }
+        else if(welche == 1) {selectedSpieler = 1;}
+        else if(welche == 2) {selectedSpieler = 2;}
+        else if(welche == 3) {selectedSpieler = 3;}
+        else if(welche == 4) {selectedSpieler = 4;}
+        
+ }  
+ void spielzug()
+ {
+     if(SpielerAnDerReihe == 1)
+    {
+        if(SpielerInHausR > 0 && augenzahl == 6 )
+        {
+            if(SpielerInHausR == 1) {Rot1.PositionSetzen(200+0*60,120+4*60); SpielerInHausR--;}
+            if(SpielerInHausR == 2) {Rot2.PositionSetzen(200+0*60,120+4*60); SpielerInHausR--;}
+            if(SpielerInHausR == 3) {Rot3.PositionSetzen(200+0*60,120+4*60); SpielerInHausR--;}
+            if(SpielerInHausR == 4) {Rot4.PositionSetzen(200+0*60,120+4*60); SpielerInHausR--;}
+        }
+        else
+        {
+            for (int i = 0; i < augenzahl; i++)
+         {
+            // if()
+            // {
+                
+            // }
+         }
+        }
+    }
+ }
+ void gehen()
+ {
+     
+ }
     }
 
 
